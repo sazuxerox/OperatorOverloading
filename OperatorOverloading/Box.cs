cc5a08430c8e00488b97eae3c4dc4531bde5e367
sazuxerox <sazzadhossain@live.com>
2015-12-20 21:30:40 +0600

    Operator overloading initial commit

---
+++ OperatorOverloading/Box.cs
@@ -0,0 +1,38 @@
+﻿namespace OperatorOverloading
+{
+    class Box
+    {
+        private double length;
+        private double breadth;
+        private double height;
+
+        public double getVolume()
+        {
+            return length*breadth*height;
+        }
+
+        public void setLength(double l)
+        {
+            length = l;
+        }
+
+        public void setBreadth(double b)
+        {
+            breadth = b;
+        }
+
+        public void setheight(double h)
+        {
+            height = h;
+        }
+
+        public static Box operator+(Box b, Box c)
+        {
+            var box = new Box();
+            box.length = b.length + c.length;
+            box.breadth = b.breadth + c.breadth;
+            box.height = b.height + c.height;
+            return box;
+        }
+    }
+}
---
+++ OperatorOverloading/Tester.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            var box1 = new Box();
+            var box2 = new Box();
+            var box3 = new Box();
+            double volume = 0.0;
+
+            //box1 specification
+            box1.setBreadth(2.0);
+            box1.setLength(1.0);
+            box1.setheight(5.0);
+
+            //box2 specification
+            box2.setBreadth(2.0);
+            box2.setLength(2.0);
+            box2.setheight(2.0);
+
+            volume = box1.getVolume();
+            Console.WriteLine("Volume of Box1 : {0:f3}", volume);
+
+            volume = box2.getVolume();
+            Console.WriteLine("Volume of Box2 : {0:f3}", volume);
+
+            box3 = box1 + box2;
+            volume = box3.getVolume();
+            Console.WriteLine("Volume of Box3: {0:f3}", volume);
+
+            Console.ReadKey();
+        }
+    }
+}
